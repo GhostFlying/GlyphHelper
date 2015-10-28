0b67bef218258c4980c2e2ad6656896e64b19234
ghostflying <GhostFlying@Gmail.com>
2015-10-28 13:11:50 +0800

    media projection lifecycle

--- app/src/main/java/com/ghostflying/glyphhelper/ScreenCapService.java
+++ app/src/main/java/com/ghostflying/glyphhelper/ScreenCapService.java
@@ -3,13 +3,14 @@ package com.ghostflying.glyphhelper;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
-
-import java.util.logging.Handler;
+import android.os.Looper;
 
 public class ScreenCapService extends Service {
     private Thread mWorkingThread;
@@ -18,6 +19,7 @@ public class ScreenCapService extends Service {
     private MediaProjection mMediaProjection;
     private int mWidth;
     private int mHeight;
+    private int mDensity;
     private ImageReader mImageReader;
 
     public ScreenCapService() {
@@ -33,11 +35,57 @@ public class ScreenCapService extends Service {
         return new ScreenCapBinder();
     }
 
-    private void setMediaProjection(MediaProjection mediaProjection){
+    private void onSetMediaProjection(MediaProjection mediaProjection, int density, int width, int height){
         mMediaProjection = mediaProjection;
+        mWidth = width;
+        mHeight = height;
+        mDensity = density;
+        mWorkingThread = new Thread(){
+            @Override
+            public void run(){
+                Looper.prepare();
+                mWorkingHandler = new Handler();
+                Looper.loop();
+            }
+        };
+        setupImageReader();
     }
 
-    public class ScreenCapBinder extends Binder{
+    private void onTearDownMediaProjection(){
+        mWorkingHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (mMediaProjection != null) {
+                    mMediaProjection.stop();
+                }
+            }
+        });
 
+        if (mWorkingThread != null){
+            mWorkingThread.interrupt();
+            mWorkingThread = null;
+        }
+    }
+
+    private void setupImageReader(){
+        mImageReader = ImageReader.newInstance(mWidth, mHeight, PixelFormat.RGBA_8888, 1);
+        mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mWorkingHandler);
+    }
+
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+
+        }
+    };
+
+    public class ScreenCapBinder extends Binder{
+        public void setMediaProjection(MediaProjection mediaProjection, int density, int width, int height){
+            onSetMediaProjection(mediaProjection, density, width, height);
+        }
+
+        public void tearDownMediaProjection(){
+            onTearDownMediaProjection();
+        }
     }
 }
